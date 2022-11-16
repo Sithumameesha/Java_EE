42c8336180d5c4a0d3e7f102f27f8f9dc6651ae0
Sithumameesha <sithumameesha76@gmail.com>
2022-11-16 10:25:51 +0530

    Content Updated

--- 01_Hello_JavaEE/src/CustomerServlet.java
+++ 01_Hello_JavaEE/src/CustomerServlet.java
@@ -7,11 +7,15 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/customer")
-public class myclass extends HttpServlet {
+public class CustomerServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter printWriter = resp.getWriter();
-        printWriter.write("Response generated");
+        String id = req.getParameter("id");
+        String name = req.getParameter("name");
+        String address = req.getParameter("address");
+        String salary = req.getParameter("salary");
+
+        System.out.println(id+" "+name+" "+address+" "+salary);
     }
 }
