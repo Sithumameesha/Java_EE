129975f30a023854b24b599f214f13e005e28c4a
Sithumameesha <sithumameesha76@gmail.com>
2022-11-24 23:14:43 +0530

    Content Updated with Html File

--- 01_Hello_JavaEE/src/myclass.java
+++ 01_Hello_JavaEE/src/myclass.java
@@ -7,15 +7,14 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet(urlPatterns = "/customer")
-public class CustomerServlet extends HttpServlet {
+public class myclass extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String id = req.getParameter("id");
+        String id = req.getParameter("cus_id");
         String name = req.getParameter("name");
         String address = req.getParameter("address");
         String salary = req.getParameter("salary");
-
         System.out.println(id+" "+name+" "+address+" "+salary);
     }
 }
---
+++ 03_JavaEE/src/servelet/CustomerServlet.java
@@ -0,0 +1,4 @@
+package servelet;
+
+public class CustomerServlet {
+}
