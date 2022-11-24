5208d72ff5b036b5e220f1118f42fe142e00c16a
Sithumameesha <sithumameesha76@gmail.com>
2022-11-25 00:02:57 +0530

    Fix Error

---
+++ 03_JavaEE/src/cus_Severlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/customer")
+public class cus_Severlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+        String name = req.getParameter("name");
+        String address = req.getParameter("address");
+        String salary = req.getParameter("salary");
+        System.out.println(id+" "+name+" "+address+" "+salary);
+        System.out.println("Hello");
+    }
+}
--- 03_JavaEE/src/servelet/CustomerServlet.java
+++
@@ -1,4 +0,0 @@
-package servelet;
-
-public class CustomerServlet {
-}
