a8621e359cd6095f31f202ab668ecb657738a262
Sithum <sithumameesha76@gmail.com>
2022-12-09 11:11:42 +0530

    Content Updated With New Module

---
+++ Mapping_Specification/src/servelet/CustomerSevelet.java
@@ -0,0 +1,24 @@
+package servelet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.*;
+import java.util.ArrayList;
+
+@WebServlet(urlPatterns = "/customer")
+public class CustomerSevelet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
