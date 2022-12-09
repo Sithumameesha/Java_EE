0383a291f6656025217bddf29a7c389607d151e9
Sithum <sithumameesha76@gmail.com>
2022-12-09 11:48:42 +0530

    Empty String Mapping

--- Mapping_Specification/src/servelet/EmptyStringMapping.java
+++ Mapping_Specification/src/servelet/EmptyStringMapping.java
@@ -5,12 +5,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException; 
-
-@WebServlet(urlPatterns = "/")
-public class defaultMapping extends HttpServlet {
+import java.io.IOException;
+@WebServlet(urlPatterns = "")
+public class EmptyStringMapping extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Default Mapping");
+        resp.getWriter().write("Empty String Mapping");
     }
 }
