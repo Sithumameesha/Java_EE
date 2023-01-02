b9f4c48272449e6d3a6302d731ddba99e093763f
Sithum <sithumameesha76@gmail.com>
2023-01-02 11:02:18 +0530

    Content Updated

--- 04_java_EE/src/cus_Severlet.java
+++ 04_java_EE/src/cus_Severlet.java
@@ -23,27 +23,27 @@ public class cus_Severlet extends HttpServlet {
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/thogakade", "root", "1234");
             PreparedStatement pstm = connection.prepareStatement("select * from customer");
             ResultSet rst = pstm.executeQuery();
-//            String jsonArray="[";
-            JsonArrayBuilder AllCustomer = Json.createArrayBuilder();
+
+            JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
             while (rst.next()) {
+                JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
 //                String id = rst.getString("id");
 //                String name = rst.getString("name");
 //                String address = rst.getString("address");
 //                double salary = rst.getDouble("salary");
-                JsonObjectBuilder customer = Json.createObjectBuilder();
-                customer.add("id",rst.getString("id"));
-                customer.add("name",rst.getString("name"));
-                customer.add("address",rst.getString("address"));
-                customer.add("salary",rst.getDouble("salary"));
-                 AllCustomer.add(customer.build());
+                objectBuilder.add("id",rst.getString("id"));
+                objectBuilder.add("name",rst.getString("name"));
+                objectBuilder.add("address",rst.getString("address"));
+                objectBuilder.add("salary",rst.getDouble("salary"));
+                arrayBuilder.add(objectBuilder.build());
 //                jsonArray+="{\"id\":\""+id+"\",\"name\":\""+name+"\",\"address\":\""+address+"\",\"salary\":"+salary+"},";
             }
-//            String finalArray = jsonArray.substring(0, jsonArray.length() - 1);
-//            finalArray+="]";
+
+
 
             resp.addHeader("Content-Type","application/json");
-//            resp.addHeader("myHeader","ijse");
-            resp.getWriter().print(AllCustomer.build());
+            resp.addHeader("myHeader","ijse");
+            resp.getWriter().print(arrayBuilder.build());
 
 //            resp.sendRedirect("customer.jsp");
 //            req.setAttribute("customers",allcustomer);
